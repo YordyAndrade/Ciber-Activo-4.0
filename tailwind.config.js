933b33fe6ae19ab3ea969c75a7cc970bb0461794
Yordy Anyerson Andrade Rodriguez <andradeyordy1118@gmail.com>
2024-11-12 04:08:49 -0500

    Subiendo mi proyecto Ciber-Activo 4.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'fondo-personalizado': "url('./src/Fondo Azul.jpg')",
+    },
+  },
+  plugins: [],
+};
+
